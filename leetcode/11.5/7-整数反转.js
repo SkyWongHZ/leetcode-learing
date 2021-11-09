2023d009300611d3da619d159eec71021513217e
Sky <skywong0571@gmail.com>
2021-11-09 11:40:59 +0800

    feat:功能代码提交

--- leetcode/11.5/7-整数反转.js
+++ leetcode/11.5/7-整数反转.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-05 15:22:15
- * @LastEditTime: 2021-11-05 15:30:37
+ * @LastEditTime: 2021-11-05 16:23:55
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /leetcode-learing/leetcode/11.5/7-整数反转.js
@@ -24,18 +24,18 @@
 const reverse=(x)=>{
     let val
     if(val>0){
-        val.reverse()
+        val=x.reverse()
     }else{
-        const opposite=Math.abs(val).reverse()
+        const opposite=Math.abs(x).reverse()
         val=-opposite
     }
     if(x<-2^31||x>2^31-1){
         x=0
     }
-    return x
+    return val
 }
 
-const x=reverse(123)
+const answer=reverse(123)
 
-console.log(x);
+console.log(answer);
 
