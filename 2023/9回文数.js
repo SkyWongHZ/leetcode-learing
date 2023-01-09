4e710afda5b010b8c8a52a971117e7bea0917384
Sky <skywong0571@gmail.com>
2023-01-09 18:00:25 +0800

    feat:回文数

--- 2023/9回文数.js
+++ 2023/9回文数.js
@@ -1,8 +1,17 @@
 /*
- * @Description  : 
+ * @Description  :  回文数
  * @Author       : Sky
  * @Date         : 2023-01-06 17:11:19
  */
 
 
 
+const isPalindrome = function(num) {
+    const  str  =num.toString().split('').reverse().join('')
+    const  newNum=Number(str)
+    const  isPalindromicNumber=num===newNum
+    return  isPalindromicNumber
+};
+
+const res= isPalindrome(2332)
+console.log('res: ', res);
